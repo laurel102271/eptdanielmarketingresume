af9b204e095abdcf03f7d5f57a1b0ccb9a81ff09
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-22 21:10:07 -0600

    Website Resume

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Marketing Resume</title>
+	</head>
+	<body>
+		<h1>Purpose</h1>
+		<p>This project will be a personal website with a focus on showing my skills to potential employers.</p>
+		<h1>Audience</h1><p>My target audience will be marketing agencies looking to hire digital maketing specialist or a freelancer.</p>
+		<h1>Goals</h1>
+		<ul>
+			<li>To acquire a job within 3 months. </li>
+			<li>To send out my resume to  all potential employers. </li>
+			<li>To make 5 phone calls per day to digital marketing agencies looking to hire. </li>
+		</ul>
+		<h1>Persona</h1>
+		<p>Name: Daniel Martinez</p>
+		<p>Age: 46.</p>
+		<p>Background: Google AdWords certified and Google Analytics certified with skills and experience in  Digital Marketing, including paid  and lead  generation,  analytics,  social  media,  display advertising, landing page optimization, and creative work. Out-of-the-box creative problem solver, capable of meeting deadlines under pressure. Detail oriented with exceptional communication skills (verbal/written). </p>
+		<p>Education:  Business Skills Institute Certified as a Network Tech, Real Estate License Holder, Google Adwords Certified Google Analytics Certified .</p>
+		<p>Fears and Frustrations:
+		<ul>
+			<li>Learning new technology skills </li>
+			<li>Very competitive job market.</li>
+			<li>Getting stuck in a job and not advancing</li>
+		</ul>
+	</body>
+</html>
