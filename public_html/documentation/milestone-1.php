ece4f1353b22dd63b5971e1b251cb4d73c79dfc3
Daniel Martinez <danny102271@sbcglobal.net>
2018-04-23 22:26:52 -0600

    Website Resume

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>Purpose</h1>
 		<p>This project will be a personal website with a focus on showing my skills to potential employers.</p>
-		<h1>Audience</h1><p>My target audience will be marketing agencies looking to hire digital maketing specialist or a freelancer.</p>
+		<h1>Audience</h1><p>My target audience will be companies looking to hire Web Developer .</p>
 		<h1>Goals</h1>
 		<ul>
 			<li>To acquire a job within 3 months. </li>
@@ -15,15 +15,12 @@
 			<li>To make 5 phone calls per day to digital marketing agencies looking to hire. </li>
 		</ul>
 		<h1>Persona</h1>
-		<p>Name: Daniel Martinez</p>
-		<p>Age: 46.</p>
-		<p>Background: Google AdWords certified and Google Analytics certified with skills and experience in  Digital Marketing, including paid  and lead  generation,  analytics,  social  media,  display advertising, landing page optimization, and creative work. Out-of-the-box creative problem solver, capable of meeting deadlines under pressure. Detail oriented with exceptional communication skills (verbal/written). </p>
-		<p>Education:  Business Skills Institute Certified as a Network Tech, Real Estate License Holder, Google Adwords Certified Google Analytics Certified .</p>
-		<p>Fears and Frustrations:
-		<ul>
-			<li>Learning new technology skills </li>
-			<li>Very competitive job market.</li>
-			<li>Getting stuck in a job and not advancing</li>
-		</ul>
+		<p>Name: Brain Bain</p>
+		<p>Age: 52.</p>
+		<p>Profession: Owner of Spectrum technology  has been in business for 10 years,and is looking to hire a full time back end Web Developer. There main focus of business is in the HealthCare Industry managing and building websites. </p>
+		<p>Technology: Uses Windows 10. Uses a toshiba laptop while in the office and at home.Has a LG 8 phone with unlimited data. Primary communication by email,phone and social networking.</p>
+		<p>Preference and Objections: Prefers the latest and greatest technology,sustainability,good customer service.Objects unproven technology,poorly designed websites.</p>
+		<p>Goals and Values: Values job security, raises and promotion,and education and a desire to self motivate.</p>
+		<p>User Story: As a job candidate I need to make this person aware of my resume for the back end Web Developer position.</p>
 	</body>
 </html>
